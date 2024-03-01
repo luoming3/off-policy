e204dc9ee7add22cf357fcd964c729a0254e1f09
Luo Mingstatus <ming.lok@outlook.com>
2024-03-01 17:43:19 +0800

    update env_core.py, adjust action space

--- offpolicy/envs/custom/env_core.py
+++ offpolicy/envs/custom/env_core.py
@@ -17,7 +17,10 @@ from .env_2d import map, plotting, Astar  # noqa: E402
 from .env_2d.car_racing import CarRacing
 # from utils.util import timethis
 
-ACT_SPACE = [[0, 0, 0], [-0.6, 0, 0], [0.6, 0, 0], [0, 0.2, 0], [0, 0, 0.8]]
+STEER_SPACE = np.linspace(-0.6, 0.6, 3)
+GAS_SPACE = np.linspace(0, 0.2, 2)
+BREAK_SPACE = np.linspace(0, 0.2, 2)
+TOTAL_DIM = len(STEER_SPACE) * len(GAS_SPACE) * len(BREAK_SPACE)
 
 
 class EnvCore(object):
@@ -25,15 +28,17 @@ class EnvCore(object):
     # 环境中的智能体
     """
 
-    def __init__(self):
+    def __init__(self, env_config={}):
         self.agent_num = 4  # number of agent
         self.obs_dim = 12  # observation dimension of agents
-        self.action_dim = 5  # set the action dimension of agents
+        # set the action dimension of agents: discrete space
+        self.action_dim = TOTAL_DIM
         self.guide_point_num = 100  # number of guide point
         self.map = map.Map()  # 2d env map
         self.width = self.map.x_range
         self.height = self.map.y_range
         self.car_env = CarRacing()
+        self.ont_hot_actions = env_config.get("one_hot_actions", True)
 
     def reset(self):
         """
@@ -42,9 +47,11 @@ class EnvCore(object):
         """
 
         # 随机的 agent 位置
-        self.car_center = np.array(self.map.random_point()).astype(float)
+        # self.car_center = np.array(self.map.random_point()).astype(float)
+        self.car_center = np.array([45., 5.])
         # 目标位置
-        self.dest = np.array(self.map.random_point())
+        # self.dest = np.array(self.map.random_point())
+        self.dest = np.array([5., 45.])
         self.last_position = self.car_center
         # reset car env
         self.car_env.reset(car_pos=self.car_center)
@@ -65,8 +72,8 @@ class EnvCore(object):
         # When self.agent_num is set to 2 agents, the input of actions is a 2-dimensional list, each list contains a shape = (self.action_dim, ) action data
         # The default parameter situation is to input a list with two elements, because the action dimension is 5, so each element shape = (5, )
         """
-        # input actions is one_hot
-        actions = self._one_hot_to_actions(actions)
+        if self.ont_hot_actions:
+            actions = self._one_hot_to_actions(actions)
         self.car_env.step(actions)
         self.car_center = np.array(self.car_env.car.hull.position)
 
@@ -87,7 +94,7 @@ class EnvCore(object):
         # Check termination conditions
         if self.is_target(car):
             sub_agent_done = [[True] for _ in range(self.agent_num)]
-            sub_agent_reward = [[np.array(1000)] for _ in range(self.agent_num)]
+            sub_agent_reward = [[np.array(10)] for _ in range(self.agent_num)]
             self.agents = []
         elif self.map.is_collision(car):
             sub_agent_done = [[True] for _ in range(self.agent_num)]
@@ -180,8 +187,23 @@ class EnvCore(object):
 
     def _one_hot_to_actions(self, one_hot_actions):
         action_index = np.argmax(one_hot_actions, axis=1)
-        actions = [ACT_SPACE[action_index[i]] for i in range(self.agent_num)]
+        actions = [[] for i in range(self.agent_num)]
+        for i in range(self.agent_num):
+            multi_actions = self._discrete_to_multidiscrete(action_index[i])
+            actions[i].append(STEER_SPACE[multi_actions[0]])
+            actions[i].append(GAS_SPACE[multi_actions[1]])
+            actions[i].append(BREAK_SPACE[multi_actions[2]])
+
         return np.array(actions)
+
+    def _discrete_to_multidiscrete(self, action_index):
+        actions = []
+        multi_discrete_act_space = [len(STEER_SPACE), len(GAS_SPACE), len(BREAK_SPACE)]
+        for space_size in multi_discrete_act_space:
+            action = action_index % space_size
+            actions.append(action)
+            action_index //= space_size
+        return actions
 
 
 # @timethis
@@ -189,7 +211,11 @@ def env_test(times=10, render=False, mode="rgb_array"):
     """
     test the validation of env
     """
-    env = EnvCore()
+    import random
+    import pprint
+
+    env_config = {"one_hot_actions": False}
+    env = EnvCore(env_config)
 
     for i in range(times):
         env.reset()
@@ -201,13 +227,15 @@ def env_test(times=10, render=False, mode="rgb_array"):
 
         step = 0
         episode_reward = 0
-        for _ in range(1000):
-            # actions = np.random.random(size=(env.agent_num,)) * 2 - 1
-            # actions = np.expand_dims(env.dest - env.car_center, 0).repeat(env.agent_num, 0) / 10
-            action_space = env.car_env.action_space
-            # actions = np.array([[0.5, 1., 0.], [-0.5, 1., 0.], [0., 0., 0.], [0., 0., 0.]])
-            actions = np.array([action_space.sample() for i in range(env.agent_num)])
-            result = env.step(actions=actions)
+        for _ in range(100):
+            actions = [[] for i in range(env.agent_num)]
+            for i in range(env.agent_num):
+                actions[i].append(random.choice(STEER_SPACE))
+                actions[i].append(random.choice(GAS_SPACE))
+                actions[i].append(random.choice(BREAK_SPACE))
+            # actions = [[0.6, 0.5, 0], [0.6, 0.5, 0], [0, 0, 0], [0, 0, 0]]
+            # pprint.pprint(actions)
+            result = env.step(actions=np.array(actions))
             if render:
                 all_frames.append(env.render())
             step += 1
