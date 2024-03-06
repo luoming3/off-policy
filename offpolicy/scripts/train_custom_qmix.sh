86b8c6f0476a60a41c249ef4c09c5e9f7ddc6874
Luo Mingstatus <ming.lok@outlook.com>
2024-03-06 14:44:08 +0800

    add config params in train_custom_qmix.sh

--- offpolicy/scripts/train_custom_qmix.sh
+++ offpolicy/scripts/train_custom_qmix.sh
@@ -15,9 +15,9 @@ for seed in $(seq ${seed_max}); do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=7 python -u ${current_dir}/train/train_custom.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
-    --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --episode_length 1024 \
+    --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --episode_length 2048 \
     --batch_size 32 --lr 7e-4 --hard_update_interval_episode 100 --num_env_steps 100000000 \
-    --n_rollout_threads 16 --buffer_size 16384 --save_interval 50000 --use_reward_normalization \
-    --use_wandb --cuda
+    --n_rollout_threads 16 --buffer_size 10000 --save_interval 50000 --epsilon_anneal_time 50000 \
+    --train_interval 100 --use_reward_normalization --use_wandb --cuda
     echo "training is done!"
 done
