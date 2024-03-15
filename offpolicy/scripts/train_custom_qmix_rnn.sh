12ebe0e860acdedba18b1463dafe484083028a49
Luo Mingstatus <ming.lok@outlook.com>
2024-03-15 17:52:53 +0800

    add script for training qmix or mqmix

--- offpolicy/scripts/train_custom_qmix_rnn.sh
+++ offpolicy/scripts/train_custom_qmix_rnn.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-env="car"
+env="custom"
 scenario="shared_7a"
 num_landmarks=3
 num_agents=4
-algo="mqmix"
-exp="el1024_bas32_nr16_use_per"
+algo="qmix"
+exp="el1024_bas32"
 seed_max=1
 
 current_dir=$(cd $(dirname $0); pwd)
@@ -16,8 +16,8 @@ for seed in $(seq ${seed_max}); do
     CUDA_VISIBLE_DEVICES=7 python -u ${current_dir}/train/train_custom.py \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
     --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --episode_length 1024 \
-    --batch_size 128 --lr 5e-4 --num_env_steps 300000000 \
-    --n_rollout_threads 16 --buffer_size 10000 --save_interval 50000 --epsilon_anneal_time 500000 \
-    --train_interval 256 --num_eval_episodes 1 --use_wandb --cuda --use_per
+    --batch_size 32 --lr 5e-4 --num_env_steps 300000000 \
+    --n_rollout_threads 1 --buffer_size 5000 --save_interval 50000 --epsilon_anneal_time 500000 \
+    --train_interval_episode 1 --num_eval_episodes 1 --use_wandb
     echo "training is done!"
 done
